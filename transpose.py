68624b20a0ee9a51c41a6af1bb0b37959430f2d3
sidduhemanth <ssiddu8619@gmail.com>
2022-11-14 15:08:12 +0530

    change1

---
+++ transpose.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 15:04:01 2022
+
+@author: cse
+"""
+
+import numpy as np
+A=np.array([[2,-2],[3.1],[1,4]])
+print("Matrix A:/n",A)
+print("Matrix A transpose:/n",np.transpose(A))
